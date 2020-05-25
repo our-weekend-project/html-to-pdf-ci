591c2a4bdcca1b93b3055c53efc84cbca05b54c9
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2020-05-25 18:04:45 -0400

    feat: use new args parser, export main function

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
 const express = require('express');
-const destPath = path.join(__dirname, '../', 'resume');
-const destFilename = 'Tim-Giles-Resume.pdf';
-const port = 5001;
-const srcResumePath = 'resume/web-resume/index.html';
-const fullResumePath = `http://localhost:${port}/${srcResumePath}`;
 
-(async () => {
-    const app = express();
-    app.use(express.static(path.join('../', '_site')));
-    const server = app.listen(port, async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        
-        await page.goto(fullResumePath, { waitUntil: 'networkidle0' });
-        await page.pdf({ 
-            path: path.join(destPath, destFilename),
-            format: 'Letter',
-            printBackground: true,
-            margin: {
-                top: '0.4 in',
-                left: '0.4 in',
-                right: '0.39 in',
-                bottom: '0.39 in'
-            }
+const main = async (program) => {
+    try {
+        const port = 5001;
+        const destPath = path.join(__dirname, program.destinationPath);
+        const fullResumePath = `http://localhost:${port}/${program.sourcePath}/${program.sourceDocument}`;
+        const destFilename = program.fileName;
+        const app = express();
+        app.use(express.static(path.join('../', program.sitePath)));
+        const server = app.listen(port, async () => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+
+            await page.goto(fullResumePath, { waitUntil: 'networkidle0' });
+            await page.pdf({
+                path: path.join(destPath, destFilename),
+                format: 'Letter',
+                printBackground: true,
+                margin: {
+                    top: '0.4 in',
+                    left: '0.4 in',
+                    right: '0.39 in',
+                    bottom: '0.39 in'
+                }
+            });
+            await browser.close();
+            server.close(() => {
+                console.log(`closing server`);
+            });
         });
-        await browser.close();
-        server.close(() => {
-            console.log(`closing server`);
-        });
-    });
-    return;
-})();
+        return;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
+
+module.exports = {
+    main
+};
